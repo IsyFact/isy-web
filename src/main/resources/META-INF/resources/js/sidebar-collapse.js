ed71377435d45eedb3c51d05f734fff2267f29d8
Stefan Dellmuth <stefan.dellmuth@msg-systems.com>
2018-03-19 09:26:19 +0100

    Unnötige Namespaces entfernt, fa-Klasse muss nicht umgeschaltet werden

--- src/main/resources/META-INF/resources/js/sidebar-collapse.js
+++ src/main/resources/META-INF/resources/js/sidebar-collapse.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     'use strict';
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
-        $('#sidebarCollapse').toggleClass('fa fa-angle-double-right');
-        $('#sidebarCollapse').toggleClass('fa fa-angle-double-left');
+        $('#sidebarCollapse').toggleClass('fa-angle-double-right');
+        $('#sidebarCollapse').toggleClass('fa-angle-double-left');
     });
 });
