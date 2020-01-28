76fc0fbbbc235acc28e137d9c5c3dc320e1b0fbc
Maike Glowik <maike.glowik@materna.de>
2020-01-28 14:50:24 +0100

    IFS-505 Aufnahme von Filter-Konfigurationen in Autoconfig

--- src/main/java/de/bund/bva/isyfact/common/web/autoconfigure/ControllerAutoConfiguration.java
+++ src/main/java/de/bund/bva/isyfact/common/web/autoconfigure/ControllerAutoConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+/**
+ * ComponentScan für die Controller von Isy-Web.
+ */
 @Configuration
 @ComponentScan(basePackages = "de.bund.bva.isyfact.common.web",
     excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX,
--- src/main/java/de/bund/bva/isyfact/common/web/autoconfigure/MvcAutoConfiguration.java
+++ src/main/java/de/bund/bva/isyfact/common/web/autoconfigure/MvcAutoConfiguration.java
@@ -1,16 +1,23 @@
 package de.bund.bva.isyfact.common.web.autoconfigure;
 
 
+
+import de.bund.bva.isyfact.aufrufkontext.http.HttpHeaderNestedDiagnosticContextFilter;
 import de.bund.bva.isyfact.common.web.exception.IsyFactFlowHandlerMapping;
 import de.bund.bva.isyfact.common.web.exception.OptimisticLockHandler;
 import de.bund.bva.isyfact.common.web.exception.common.AusnahmeIdMapper;
 import de.bund.bva.isyfact.common.web.global.GlobalFlowController;
+import de.bund.bva.isyfact.util.spring.MessageSourceHolder;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.faces.mvc.JsfView;
 import org.springframework.faces.webflow.JsfFlowHandlerAdapter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.RequestContextFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -27,7 +34,16 @@ public class MvcAutoConfiguration implements WebMvcConfigurer {
     // <mvc:resources>
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/").setCachePeriod(86400);
+        registry.addResourceHandler("/resources/**")
+            .addResourceLocations("/")
+            .setCachePeriod(86400);
+    }
+
+    //Speichert die Message-Source fuer statische Zugriffe auf Messages.
+    @Bean
+    @ConditionalOnMissingBean
+    public MessageSourceHolder messageSourceHolder() {
+        return new MessageSourceHolder();
     }
 
     @Bean
@@ -60,6 +76,39 @@ public class MvcAutoConfiguration implements WebMvcConfigurer {
     @Bean
     public OptimisticLockHandler optimisticLockHandler(GlobalFlowController globalFlowController) {
         return new OptimisticLockHandler(globalFlowController);
+    }
+
+    //====== Filter-Config
+    //Request Scoping
+    @Bean
+    FilterRegistrationBean<RequestContextFilter> requestContextFilter() {
+        FilterRegistrationBean<RequestContextFilter> registrationBean =
+            new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RequestContextFilter());
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
+
+    //Automatisches setzen der Korrelations-ID
+    @Bean
+    FilterRegistrationBean<HttpHeaderNestedDiagnosticContextFilter> httpHeaderNestedDiagnosticContextFilter() {
+        FilterRegistrationBean<HttpHeaderNestedDiagnosticContextFilter> registrationBean =
+            new FilterRegistrationBean<>();
+        registrationBean.setFilter(new HttpHeaderNestedDiagnosticContextFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
+
+    //Character-Encoding auf UTF-8
+    @Bean
+    FilterRegistrationBean<CharacterEncodingFilter> characterEncodingFilter() {
+        FilterRegistrationBean<CharacterEncodingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CharacterEncodingFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.addInitParameter("encoding", "UTF-8");
+        registrationBean.addInitParameter("forceEncoding", "true");
+        return registrationBean;
     }
 
 }
