e5e7d670f4ebb6166e1015cb1bf4126fc955849b
Maike Glowik <maike.glowik@materna.de>
2020-12-10 16:04:10 +0100

    IFE-141: global export in Objekt isywebjs verschoben

--- src/main/js/common/global-exports.js
+++ src/main/js/common/global-exports.js
@@ -5,7 +5,6 @@ import { blockSingleButton } from '../widgets/buttons';
 // they are bundled inside an "isywebjs" object, to avoid overly polluting the window-namespace
 const isywebjs = {};
 isywebjs.listpickerAjaxReload = listpickerAjaxReload;
+isywebjs.blockSingleButton = blockSingleButton;
 
 window.isywebjs = isywebjs;
-
-window.blockSingleButton = blockSingleButton;
