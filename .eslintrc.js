f5a8703d845360e315bfb1b816fdbfe3c9fb8950
mglowik <maike.glowik@materna.de>
2021-03-11 19:00:39 +0100

    IFE-141 ergaenze ESLint-Config fuer IE11-support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+    "plugins": ["es"],
     "env": {
         "browser": true,
         "es6": true,
@@ -7,5 +8,15 @@ module.exports = {
     "parserOptions": {
         "ecmaVersion": 6,
         "sourceType": "module"
+    },
+    //for ie11-compatibility, can be removed once we drop ie11 support
+    "extends": [
+        "plugin:es/no-new-in-es2015"
+    ],
+    "rules": {
+        //modules are resolved by webpack, so we can use them in our code
+        "es/no-modules": "off",
+        //ie11 does support block-scoped variables, so we can use them
+        "es/no-block-scoped-variables": "off"
     }
 };
