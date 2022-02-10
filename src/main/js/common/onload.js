5ed0a0146c0cd16c2ec731cace5e47a99dfd589e
szabel <stefanie.zabel@materna.de>
2022-02-10 09:05:28 +0100

    refactor: IFE-205 onload.js restructure focusOnload

--- src/main/js/common/onload.js
+++ src/main/js/common/onload.js
@@ -14,6 +14,7 @@ import { bindReturnToDefaultButton } from '../widgets/buttons';
 import { applyMask, deletePlaceholdersOnReturn } from '../widgets/inputmask';
 import { initNavigation } from './tastatursteuerung-navigation';
 import { initSelectlists } from '../widgets/selectlist';
+import { focusOnload } from './focusOnload';
 
 $(document).ready(function () {
     'use strict';
@@ -179,46 +180,7 @@ function refreshFunctions() {
     // The element to be focussed can be overwritten or deactivated using the isy:focusOnload tag.
     // Additionally the focusOnloadForce id can be used to force a specific focus element.
     // --------------------------------------------------------
-
-    const $focusOnloadActive = $("[id$='focusOnloadActive']").last();
-    let $focusOnloadDeactivated;
-    let $focusOnloadForce;
-    const focusOnloadElement = "[id$='focusOnloadElement']";
-
-    if ($('#modal-add-personal').val() === undefined) {
-
-        $focusOnloadDeactivated = $('#inhaltsbereichForm').find("[id$='focusOnloadDeactivated']");
-        $focusOnloadForce = $('#inhaltsbereichForm').find("[id$='focusOnloadForce']");
-
-        if ($focusOnloadDeactivated.val() != 'true') {
-            if ($focusOnloadActive.val() == 'true' || $focusOnloadForce.val() == 'true') {
-                $focusOnloadActive.val('false');
-
-                if ($('#inhaltsbereichForm').find(focusOnloadElement).val() === undefined) {
-                    // use default value
-                    $('#inhaltsbereichForm').find('input:not([type=hidden]), a:not([tabindex=-1]), button').first().focus();
-                } else {
-                    $('#inhaltsbereichForm').find($('#inhaltsbereichForm').find(focusOnloadElement).val()).first().focus();
-                }
-            }
-        }
-    } else {
-        $focusOnloadDeactivated = $('#modalDialogPlaceholderForm').find("[id$='focusOnloadDeactivated']");
-        $focusOnloadForce = $('#modalDialogPlaceholderForm').find("[id$='focusOnloadForce']");
-
-        if ($focusOnloadDeactivated.val() != 'true') {
-            if ($focusOnloadActive.val() == 'true' || $focusOnloadForce.val() == 'true') {
-                $focusOnloadActive.val('false');
-
-                if ($('#modalDialogPlaceholderForm').find(focusOnloadElement).val() === undefined) {
-                    // use default value
-                    $('#modalDialogPlaceholderForm').find('input:not([type=hidden]), a:not([tabindex=-1]), button').first().focus();
-                } else {
-                    $('#modalDialogPlaceholderForm').find($('#modalDialogPlaceholderForm').find(focusOnloadElement).val()).first().focus();
-                }
-            }
-        }
-    }
+    focusOnload();
 
     // --------------------------------------------------------
     // Activate functionalities for a datatable
