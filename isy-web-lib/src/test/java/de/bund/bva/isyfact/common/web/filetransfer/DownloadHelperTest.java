3c75c59450b5b837169dfcf75266c2ed69e3dfba
Stefan Fink (Seitenbau) <stefan.fink@seitenbau.com>
2022-06-15 14:00:20 +0200

    fix: IFS-1155: correctly encode files with empty space in the name for download

---
+++ isy-web-lib/src/test/java/de/bund/bva/isyfact/common/web/filetransfer/DownloadHelperTest.java
@@ -0,0 +1,57 @@
+package de.bund.bva.isyfact.common.web.filetransfer;
+
+import static org.mockito.Mockito.RETURNS_DEFAULTS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import javax.faces.FactoryFinder;
+import javax.faces.context.ExternalContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.sun.faces.config.WebConfiguration;
+import com.sun.faces.context.FacesContextImpl;
+import com.sun.faces.lifecycle.LifecycleImpl;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DownloadHelperTest {
+
+    private final DownloadHelper sut = new DownloadHelper();
+
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    private ExternalContext externalContext;
+
+    @Before
+    public void mockFacesContext() {
+        when(externalContext.getRequest())
+                .thenReturn(mock(HttpServletRequest.class, RETURNS_DEFAULTS));
+        when(externalContext.getApplicationMap().get("com.sun.faces.config.WebConfiguration"))
+                .thenReturn(mock(WebConfiguration.class, RETURNS_DEFAULTS));
+
+        FactoryFinder.setFactory(FactoryFinder.RENDER_KIT_FACTORY, "com.sun.faces.renderkit.RenderKitFactoryImpl");
+        new FacesContextImpl(externalContext, new LifecycleImpl());
+    }
+
+    @Test
+    public void starteDownload_emptyStringInFilename() {
+        // prepare
+        final FileModel input = new FileModel();
+        input.setDateiname("a b.txt");
+        input.setInhalt(new byte[0]);
+        input.setMimeType("plain/text");
+
+        // execute
+        sut.starteDownload(input);
+
+        // verify
+        verify(externalContext).setResponseHeader(
+                "Content-Disposition", "attachment;filename=\"a%20b.txt\"; filename*=UTF-8''a%20b.txt");
+    }
+}
