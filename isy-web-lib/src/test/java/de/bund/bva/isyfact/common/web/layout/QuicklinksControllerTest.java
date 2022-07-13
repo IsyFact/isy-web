c3469709f8f66d5bc573027569923af0dd0b48c0
Fabian Perl <fabian.perl@seitenbau.com>
2022-07-13 07:57:42 +0200

    test: IFS-1616: adjusts test cases to new spring version

--- isy-web-lib/src/test/java/de/bund/bva/isyfact/common/web/layout/QuicklinksControllerTest.java
+++ isy-web-lib/src/test/java/de/bund/bva/isyfact/common/web/layout/QuicklinksControllerTest.java
@@ -7,7 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.webflow.context.ExternalContext;
 import org.springframework.webflow.context.ExternalContextHolder;
 import org.springframework.webflow.core.collection.SharedAttributeMap;
